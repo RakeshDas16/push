fbd434ae55e9bd42758e50f10ecbba9a12678589
Rakesh Das <imrakesh.gir@gmail.com>
2020-11-22 23:22:46 +0530

    initial

---
+++ push.js
@@ -0,0 +1,6 @@
+var push=require('web-push');
+
+let vapidKeys={
+    publicKey: 'BPGIzs3SBrp1ZYMtTvU4GBkSUQBPDc6qrl34CdEOaCScQXCwvAcHCHpM3A6haLAU_r5xmT5UiCneobG8nuW1qq0',
+    privateKey: 'm3ciBmtcsaVz4jT-LvzZM19IM74aNt64xdabXWKVGGE'
+  }
